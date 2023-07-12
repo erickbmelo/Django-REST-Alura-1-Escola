3189d71db7dc81ecaf0384bc6747896adf6c36ea
Erick de Brito Melo <erick.melo@sorocaba.sp.gov.br>
2023-07-12 18:56:39 -0300

    Serializando a Matricula

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from escola.models import Aluno, Curso
-from escola.serializer import AlunoSerializer, CursoSerializer
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
 
 
 class AlunosViewSet(viewsets.ModelViewSet):
@@ -12,3 +12,8 @@ class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """Exibindo todas as matrículas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
